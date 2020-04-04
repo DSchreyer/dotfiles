4087364d968e895b86ce021094a93004da175ebd
DSchreyer <daniel-schreyer@web.de>
2020-04-04 10:22:16 +0200

    Changed rofi config. Added two monitor script

--- i3/scripts/two_monitors.sh
+++ i3/scripts/two_monitors.sh
@@ -1,3 +1,3 @@
 #/usr/bin/bash
-sleep 5
-xrandr | grep "HDMI1 connected" && xrandr --output eDP1 --auto --output HDMI1 --auto --right-of eDP1 && i3-msg "workspace 3: , move workspace to output HDMI1" && i3-msg "workspace 4: , move workspace to output HDMI1"
+sleep 10
+xrandr | grep "HDMI1 connected" && xrandr --output eDP1 --auto --output HDMI1 --auto --right-of eDP1 && i3-msg "workspace 7: , move workspace to output HDMI1" && i3-msg "workspace 4: , move workspace to output HDMI1" && i3-msg "workspace 1: , move workspace to output HDMI1"
